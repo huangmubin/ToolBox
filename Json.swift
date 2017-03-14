dd84b9e814fee868540cfa614d623efd9c14858f
huangmubin <hmb87381041@163.com>
2017-03-14 11:00:02 +0800

    Create a json.swift, using to easy analysis JSON data.

---
+++ Json.swift
@@ -0,0 +1,16 @@
+//
+//  Json.swift
+//  JsonProject
+//
+//  Created by 黄穆斌 on 2017/3/14.
+//  Copyright © 2017年 MuBinHuang. All rights reserved.
+//
+//  A json analysis tool class.
+
+import Foundation
+
+class Json {
+
+    
+    
+}
