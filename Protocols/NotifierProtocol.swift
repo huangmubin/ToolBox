5b4b22d37e77291482cf7d69e5da3752a9d75366
huangmubin <hmb87381041@163.com>
2017-03-17 20:56:44 +0800

    create NorifierProtocol.swift, a Notification Tool.

---
+++ Protocols/NotifierProtocol.swift
@@ -0,0 +1,76 @@
+//
+//  NotifierProtocol.swift
+//  NetworkTestProject
+//
+//  Created by 黄穆斌 on 2017/3/17.
+//  Copyright © 2017年 MuBinHuang. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Notifier Ptorocol
+
+/**
+ Easy notifier method.
+ */
+public protocol NotifierProtocol { }
+extension NotifierProtocol {
+    
+    /** Observer a notify. */
+    func observer(name: NSNotification.Name, selector: Selector, object: Any? = nil) {
+        NotificationCenter.default.addObserver(self, selector: selector, name: name, object: object)
+    }
+    
+    /** remove observer a notify. */
+    func unobserver(name: NSNotification.Name? = nil, object: Any? = nil) {
+        NotificationCenter.default.removeObserver(self, name: name, object: object)
+    }
+    
+    /** Post a notify. */
+    func post(name: NSNotification.Name, infos: [AnyHashable: Any]? = nil) {
+        NotificationCenter.default.post(name: name, object: self, userInfo: infos)
+    }
+    
+    /** Post a notify in specific queue. */
+    func post(name: NSNotification.Name, infos: [AnyHashable: Any]? = nil, inQueue: DispatchQueue) {
+        inQueue.async {
+            NotificationCenter.default.post(name: name, object: self, userInfo: infos)
+        }
+    }
+}
+
+// MARK: - Notify Object
+
+extension Notification {
+    
+    private func open_info(keys: Any...) -> Any? {
+        if let infos = userInfo {
+            var temp: Any? = infos
+            for key in keys {
+                if let json = temp as? [String: Any],
+                    let key = key as? String {
+                    temp = json[key]
+                }
+                else if let json = temp as? [Any],
+                    let key = key as? Int {
+                    temp = json[key]
+                }
+            }
+            return temp
+        }
+        return nil
+    }
+    
+    /**
+     Get the data with key.
+     - parameter keys: some String or Int key. like [1, "key", 2]
+     - returns: the T type data, if nill return the type
+     */
+    public func get<T>(_ keys: Any..., type: T) -> T {
+        if let value = open_info(keys: keys) as? T {
+            return value
+        }
+        return type
+    }
+    
+}
