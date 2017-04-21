b5007ed70a2df8cf6e9ead7a0c3e3175c94dd6f5
Myron <hmb87381041@163.com>
2017-04-21 10:46:07 +0800

    Update Alert, but not test.

--- ToolBoxProject/ToolBoxProject/NewViewController.swift
+++ ToolBoxProject/ToolBoxProject/NewViewController.swift
@@ -1,29 +1,27 @@
 //
-//  ViewController.swift
+//  NewViewController.swift
 //  ToolBoxProject
 //
-//  Created by Myron on 2017/4/20.
+//  Created by Myron on 2017/4/21.
 //  Copyright © 2017年 Myron. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("New View Controller: View Did Appear.")
     }
-
-
+    
+    deinit {
+        print("New View Controller: \(self) is deinit.")
+    }
+    
 }
-
--- UIKit/Alert.swift
+++ UIKit/Alert.swift
@@ -8,10 +8,143 @@
 
 import UIKit
 
+// MARK: - Alert Tools
+
 class Alert {
     
-    class func show(_ controller: UIViewController, title: String?, message: String, time: TimeInterval) {
+    let alert: UIAlertController
+    
+    
+    // MARK: Init
+    
+    init(title: String? = nil, message: String? = nil, style: UIAlertControllerStyle = UIAlertControllerStyle.alert) {
+        alert = UIAlertController(title: title, message: message, preferredStyle: style)
+    }
+    
+    func show(_ controller: UIViewController, time: TimeInterval = 0) {
+        if let textFields = alert.textFields {
+            for text in textFields {
+                text.text = nil
+            }
+        }
+        controller.present(alert, animated: true, completion: {
+            if time > 0 {
+                DispatchQueue.global().async {
+                    Thread.sleep(forTimeInterval: time)
+                    DispatchQueue.main.async {
+                        self.alert.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+        })
+    }
+    
+    // MARK: Info
+    
+    func title(_ value: String) -> Alert {
+        alert.title = value
+        return self
+    }
+    
+    func message(_ value: String) -> Alert {
+        alert.message = value
+        return self
+    }
+    
+    // MARK: Action
+    
+    func action(title: String? = NSLocalizedString("Sure", comment: "Sure"), handler: ((UIAlertAction) -> Void)?) -> Alert {
+        let action = UIAlertAction(title: title, style: .default, handler: handler)
+        alert.addAction(action)
+        return self
+    }
+    
+    func cancel(title: String? = NSLocalizedString("Cancel", comment: "Cancel"), handler: ((UIAlertAction) -> Void)?) -> Alert {
+        let action = UIAlertAction(title: title, style: .cancel, handler: handler)
+        alert.addAction(action)
+        return self
+    }
+    
+    func destructive(title: String?, handler: ((UIAlertAction) -> Void)?) -> Alert {
+        let action = UIAlertAction(title: title, style: .destructive, handler: handler)
+        alert.addAction(action)
+        return self
+    }
+    
+    // MARK: Text Filed
+    
+    var textFiledBeginEditings: [UITextField: (UITextField) -> Void] = [:]
+    var textFiledDidChanges: [UITextField: (UITextField) -> Void] = [:]
+    var textFiledEndEditings: [UITextField: (UITextField) -> Void] = [:]
+    
+    func textField(placeholder: String?, hanlder: ((UITextField) -> Void)?) -> Alert {
+        alert.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = placeholder
+            hanlder?(textField)
+            NotificationCenter.default.addObserver(self, selector: #selector(self.textFieldTextDidEndEditing), name: NSNotification.Name.UITextFieldTextDidBeginEditing, object: textField)
+            NotificationCenter.default.addObserver(self, selector: #selector(self.textFieldTextDidChange), name: NSNotification.Name.UITextFieldTextDidChange, object: textField)
+            NotificationCenter.default.addObserver(self, selector: #selector(self.textFieldTextDidBeginEditing), name: NSNotification.Name.UITextFieldTextDidEndEditing, object: textField)
+        })
+        return self
+    }
+    
+    func beginEditing(_ hanlder: @escaping (UITextField) -> Void) -> Alert {
+        if let text = alert.textFields?.last {
+            textFiledBeginEditings[text] = hanlder
+        }
+        return self
+    }
+    func changed(_ hanlder: @escaping (UITextField) -> Void) -> Alert {
+        if let text = alert.textFields?.last {
+            textFiledDidChanges[text] = hanlder
+        }
+        return self
+    }
+    func endEditing(_ hanlder: @escaping (UITextField) -> Void) -> Alert {
+        if let text = alert.textFields?.last {
+            textFiledEndEditings[text] = hanlder
+        }
+        return self
+    }
+    
+    func clear() {
+        textFiledBeginEditings.removeAll()
+        textFiledDidChanges.removeAll()
+        textFiledEndEditings.removeAll()
+    }
+    
+    // MARK: Notification
+    
+    @objc func textFieldTextDidBeginEditing(notification: Notification) {
+        if let text = notification.object as? UITextField {
+            if let hanlder = textFiledBeginEditings[text] {
+                hanlder(text)
+            }
+        }
+    }
+    @objc func textFieldTextDidChange(notification: Notification) {
+        if let text = notification.object as? UITextField {
+            if let hanlder = textFiledDidChanges[text] {
+                hanlder(text)
+            }
+        }
+    }
+    @objc func textFieldTextDidEndEditing(notification: Notification) {
+        if let text = notification.object as? UITextField {
+            if let hanlder = textFiledEndEditings[text] {
+                hanlder(text)
+            }
+        }
+    }
+}
+
+// MARK: - Alert Simple Tools
+
+extension Alert {
+    
+    class func show(_ controller: UIViewController, title: String?, message: String?, time: TimeInterval) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
         controller.present(alert, animated: true, completion: nil)
         DispatchQueue.global().async {
             Thread.sleep(forTimeInterval: time)
@@ -21,14 +154,14 @@ class Alert {
         }
     }
     
-    class func show(_ controller: UIViewController, title: String?, message: String, button: String?, action: ((UIAlertAction) -> Void)?) {
+    class func show(_ controller: UIViewController, title: String?, message: String?, button: String?, action: ((UIAlertAction) -> Void)?) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let info = UIAlertAction(title: button, style: .default, handler: action)
         alert.addAction(info)
         controller.present(alert, animated: true, completion: nil)
     }
     
-    class func show(_ controller: UIViewController, title: String?, message: String, sure: String?, sureAction: ((UIAlertAction) -> Void)?, cancel: String?, cancelAction: ((UIAlertAction) -> Void)?) {
+    class func show(_ controller: UIViewController, title: String?, message: String?, sure: String?, sureAction: ((UIAlertAction) -> Void)?, cancel: String?, cancelAction: ((UIAlertAction) -> Void)?) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let sInfo = UIAlertAction(title: sure, style: .default, handler: sureAction)
         alert.addAction(sInfo)
@@ -39,7 +172,7 @@ class Alert {
         controller.present(alert, animated: true, completion: nil)
     }
     
-    class func push(_ controller: UIViewController, item: UIView?, title: String?, message: String, actions: [(String, UIAlertActionStyle, ((UIAlertAction) -> Void)?)]) {
+    class func push(_ controller: UIViewController, item: UIView?, title: String?, message: String?, actions: [(String, UIAlertActionStyle, ((UIAlertAction) -> Void)?)]) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
         alert.modalPresentationStyle = .popover
         for action in actions {
