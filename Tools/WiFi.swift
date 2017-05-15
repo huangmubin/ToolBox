e51aec49b25f386e9ecf30b4504aa50d5c325e94
Myron <hmb87381041@163.com>
2017-05-15 16:21:37 +0800

    update

---
+++ Tools/WiFi.swift
@@ -0,0 +1,61 @@
+import SystemConfiguration.CaptiveNetwork
+
+class WiFi {
+    
+    /**
+     获取 WiFi 名称跟 Mac 信息。
+     */
+    class func infos() -> (ssid: String, mac: String) {
+        if let cfas: NSArray = CNCopySupportedInterfaces() {
+            for cfa in cfas {
+                if let dict = CFBridgingRetain(
+                    CNCopyCurrentNetworkInfo(cfa as! CFString)
+                    ) {
+                    if let ssid = dict["SSID"] as? String,
+                    let mac = dict["BSSID"] as? String {
+                        return (ssid, mac)
+                    }
+                }
+            }
+        }
+        return ("Unknow", "Unknow")
+    }
+
+    /**
+     获取 WiFi 名称。
+     */
+    class func ssid() -> String? {
+        if let cfas: NSArray = CNCopySupportedInterfaces() {
+            for cfa in cfas {
+                if let dict = CFBridgingRetain(
+                    CNCopyCurrentNetworkInfo(cfa as! CFString)
+                    ) {
+                    if let ssid = dict["SSID"] as? String {
+                        return ssid
+                    }
+                }
+            }
+        }
+        return nil
+    }
+
+
+    /**
+     获取 Mac 信息。
+     */
+    class func mac() -> String? {
+        if let cfas: NSArray = CNCopySupportedInterfaces() {
+            for cfa in cfas {
+                if let dict = CFBridgingRetain(
+                    CNCopyCurrentNetworkInfo(cfa as! CFString)
+                    ) {
+                    if let mac = dict["BSSID"] as? String {
+                        return ssid, mac
+                    }
+                }
+            }
+        }
+        return nil
+    }
+
+}
