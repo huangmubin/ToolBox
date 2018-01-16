4db6a7fe0b8ee2bd4d81d480c2b1f07ec4d8efb9
Myron <hmb87381041@163.com>
2018-01-16 14:32:39 +0800

    add Ex_UIFont

---
+++ Extension/Ex_UIFont.swift
@@ -0,0 +1,18 @@
+//
+//  Ex_Font.swift
+//  MyCalculator
+//
+//  Created by Myron on 2018/1/16.
+//  Copyright © 2018年 Myron. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    /** Label font */
+    class func label_font() -> UIFont {
+        return UIFont.systemFont(ofSize: UIFont.labelFontSize)
+    }
+    
+}
