8777edbad974b6f5f576c029e4f10a7652ab1200
Myron MacBook Pro <hmb87381041@163.com>
2018-01-19 00:17:45 +0800

    update TextView

--- Extension/Ex_UITextView.swift
+++ Extension/Ex_UITextView.swift
@@ -11,13 +11,19 @@ import UIKit
 extension UITextView {
     
     /** Get the range in visible text */
-    public func visible_text_range() -> NSRange {
+    public func visible_text_range() -> Range<Int> {
         let start = closestPosition(to: contentOffset) ?? beginningOfDocument
         let end = closestPosition(to: visible_rect().max_point()) ?? endOfDocument
-        return NSRange(
-            location: offset(from: beginningOfDocument, to: start),
-            length: offset(from: start, to: end)
-        )
+        return offset(from: beginningOfDocument, to: start) ..< offset(from: beginningOfDocument, to: end)
+    }
+    
+    /** Get the string index range in visible */
+    public func visible_range() -> Range<String.Index>? {
+        guard let text = text else { return nil }
+        
+        let start = closestPosition(to: contentOffset) ?? beginningOfDocument
+        let ended = closestPosition(to: visible_rect().max_point()) ?? endOfDocument
+        return text.index(text.startIndex, offsetBy: offset(from: beginningOfDocument, to: start)) ..< text.index(text.startIndex, offsetBy: offset(from: beginningOfDocument, to: ended))
     }
     
 }
